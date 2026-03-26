db83e061f2398ee8f92aaea484e194a4ab05eac8
Hanane Challal <hananchallal07@gmail.com>
2026-03-26 20:17:10 +0100

    mobile-update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
-import React,{useEffect} from "react";
+import React,{useEffect,useState} from "react";
 function Header({ theme, toggleTheme }) {
+    const [menuOpen, setMenuOpen] = useState(false);
+
   useEffect(() => {
   const header = document.querySelector('header');
 
   const handleScroll = () => {
-    if (window.scrollY > 50) { // 50px ou ce que tu veux
+    if (window.scrollY > 50) { 
       header.classList.add('scrolled');
     } else {
       header.classList.remove('scrolled');
@@ -29,7 +31,13 @@ function Header({ theme, toggleTheme }) {
   <ion-icon name="moon" class="moon"></ion-icon>
   <ion-icon name="sunny" class="sun"></ion-icon>
 </button>
-            <button className="nav-menu-btn">
+            {/* <button className="nav-menu-btn">
+              <ion-icon name="menu-outline"></ion-icon>
+            </button> */}
+            <button
+              className="nav-menu-btn"
+              onClick={() => setMenuOpen(true)}
+            >
               <ion-icon name="menu-outline"></ion-icon>
             </button>
           </div>
@@ -53,6 +61,41 @@ function Header({ theme, toggleTheme }) {
 
         </nav>
       </div>
+
+
+      {/* ✅ MOBILE NAV */}
+      <nav className={`mobile-nav ${menuOpen ? "active" : ""}`}>
+
+        <button
+          className="nav-close-btn"
+          onClick={() => setMenuOpen(false)}
+        >
+          <ion-icon name="close-outline"></ion-icon>
+        </button>
+
+        <div className="wrapper">
+          <p className="nav-title">Menu</p>
+
+          <ul>
+            <li className="nav-item">
+              <a href="#home" className="nav-link" onClick={() => setMenuOpen(false)}>Home</a>
+            </li>
+            <li className="nav-item">
+              <a href="#projects" className="nav-link" onClick={() => setMenuOpen(false)}>Projects</a>
+            </li>
+            <li className="nav-item">
+              <a href="#education" className="nav-link" onClick={() => setMenuOpen(false)}>Education</a>
+            </li>
+            <li className="nav-item">
+              <a href="#certificates" className="nav-link" onClick={() => setMenuOpen(false)}>Certificates</a>
+            </li>
+            <li className="nav-item">
+              <a href="#contact" className="nav-link" onClick={() => setMenuOpen(false)}>Contact</a>
+            </li>
+          </ul>
+        </div>
+
+      </nav>
     </header>
   );
 }
