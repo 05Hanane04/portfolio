cee4cac50bfa8c595f1580ae83baae213b3f1af4
Hanane Challal <hananchallal07@gmail.com>
2026-04-01 22:10:21 +0100

    lastCommit

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,52 +1,102 @@
 function Marquee() {
   return (
+    <section className="marquee-section">
+      <div className="marquee">
+        <div className="marquee-track">
+          <div className="marquee-group">
+            <span>
+              <i className="fa-brands fa-react"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-js"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-php"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-laravel"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-symfony"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-database"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-html5"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-css3-alt"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-vuejs"></i>
+            </span>
 
-<section className="marquee-section">
+            <span>
+              <i className="fa-brands fa-python"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-bootstrap"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-git-alt"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-github"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-figma"></i>
+            </span>
+          </div>
 
-<div className="marquee">
+          <div className="marquee-group">
+            <span>
+              <i className="fa-brands fa-react"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-js"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-php"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-laravel"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-symfony"></i>
+            </span>
+            <span>
+              <i className="fa-solid fa-database"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-html5"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-css3-alt"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-vuejs"></i>
+            </span>
 
-<div className="marquee-track">
-
-<div className="marquee-group">
-
-<span><i className="fa-brands fa-react"></i></span>
-<span><i className="fa-brands fa-js"></i></span>
-<span><i className="fa-brands fa-php"></i></span>
-<span><i className="fa-brands fa-laravel"></i></span>
-<span><i className="fa-brands fa-symfony"></i></span>
-<span><i className="fa-solid fa-database"></i></span>
-<span><i className="fa-brands fa-html5"></i></span>
-<span><i className="fa-brands fa-css3-alt"></i></span>
-<span><i className="fa-brands fa-python"></i></span>
-<span><i className="fa-brands fa-bootstrap"></i></span>
-<span><i className="fa-brands fa-github"></i></span>
-<span><i className="fa-brands fa-figma"></i></span>
-
-</div>
-
-<div className="marquee-group">
-
-<span><i className="fa-brands fa-react"></i></span>
-<span><i className="fa-brands fa-js"></i></span>
-<span><i className="fa-brands fa-php"></i></span>
-<span><i className="fa-brands fa-laravel"></i></span>
-<span><i className="fa-brands fa-symfony"></i></span>
-<span><i className="fa-solid fa-database"></i></span>
-<span><i className="fa-brands fa-html5"></i></span>
-<span><i className="fa-brands fa-css3-alt"></i></span>
-<span><i className="fa-brands fa-python"></i></span>
-<span><i className="fa-brands fa-bootstrap"></i></span>
-<span><i className="fa-brands fa-github"></i></span>
-<span><i className="fa-brands fa-figma"></i></span>
-
-</div>
-
-</div>
-
-</div>
-
-</section>
-
+            <span>
+              <i className="fa-brands fa-python"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-bootstrap"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-git-alt"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-github"></i>
+            </span>
+            <span>
+              <i className="fa-brands fa-figma"></i>
+            </span>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
